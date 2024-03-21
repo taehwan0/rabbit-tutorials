14d5ca0dd569f6da7bdeb06e3d7030da0c12d15e
taehwan0 <jang_taehwan@naver.com>
2024-03-21 16:15:36 +0900

    init: git init
    - fasoo rabbit mq강의

---
+++ src/test/java/com/example/mqtutorials/MqTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mqtutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqTutorialsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
