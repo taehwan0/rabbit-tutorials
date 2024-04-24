9c93166f1c0eb57ad2161e58218954f8e84535bf
taehwan0 <jang_taehwan@naver.com>
2024-04-24 10:27:22 +0900

    fix: message converter 동작하지 않는 오류 수정

--- src/main/java/com/example/mqtutorials/config/RabbitConfig.java
+++ src/main/java/com/example/mqtutorials/config/RabbitConfig.java
@@ -9,10 +9,8 @@ import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFacto
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.ContentTypeDelegatingMessageConverter;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.amqp.support.converter.SimpleMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,21 +23,15 @@ public class RabbitConfig {
     }
 
     @Bean
-    public MessageConverter messageConverter() {
-        ContentTypeDelegatingMessageConverter converter = new ContentTypeDelegatingMessageConverter(new Jackson2JsonMessageConverter());
-
-        SimpleMessageConverter simpleMessageConverter = new SimpleMessageConverter();
-        converter.addDelegate("text/plain", simpleMessageConverter);
-        converter.addDelegate(null, simpleMessageConverter);
-
-        return converter;
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 
     @Bean
-    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
         SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
         factory.setConnectionFactory(connectionFactory);
-        factory.setMessageConverter(messageConverter);
+        factory.setMessageConverter(jackson2JsonMessageConverter());
         factory.setPrefetchCount(3);
         factory.setConcurrentConsumers(3);
         factory.setMaxConcurrentConsumers(3);
--- src/main/java/com/example/mqtutorials/listener/ChatListener.java
+++ src/main/java/com/example/mqtutorials/listener/ChatListener.java
@@ -3,7 +3,6 @@ package com.example.mqtutorials.listener;
 import com.example.mqtutorials.domain.Chat;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -11,17 +10,10 @@ import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.stereotype.Component;
 
 @Slf4j
-@RabbitListener(queues = "user.buddy")
 @Component
 public class ChatListener {
 
-    @RabbitHandler
-    public void receiveString(String input) {
-        log.info(" [x] input message is instanceof: {}", input.getClass());
-        log.info(" [x] 'String' input: {}", input);
-    }
-
-    @RabbitHandler
+    @RabbitListener(queues = "user.buddy")
     public void receiveChat(
             Chat in,
             @Headers Map<String, Object> headers,
