1e343c994691fa5f1f2996f2cde58aebc94e30fd
taehwan0 <jang_taehwan@naver.com>
2024-04-24 10:10:32 +0900

    fix: 3강 과제 수정

--- src/main/java/com/example/mqtutorials/controller/RabbitAuthController.java
+++ src/main/java/com/example/mqtutorials/controller/RabbitAuthController.java
@@ -10,12 +10,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Profile("server")
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/rabbit/auth")
@@ -32,7 +34,7 @@ public class RabbitAuthController {
         log.info("rabbit auth user info: {}", request);
 
         if ("buddy".equals(request.getUsername())) {
-            return "allow";
+            return "allow administrator";
         }
         return "deny";
     }
@@ -49,48 +51,56 @@ public class RabbitAuthController {
 
     @PostMapping(value = "/resource")
     public String postResource(ResourcePathRequest request) {
-        log.info("rabbit auth vhost info: {}", request);
+        // exchange "request" 에 대해서 허용 케이스
+        if ("exchange".equals(request.getResource()) && "request".equals(request.getName())) {
+            // 허용하는 permission은 publish뿐
+            List<String> exchangeAllowPermissions = Arrays.asList("write");
 
-        // vhost_chat 으로의 접근만 허용
-        if ("chat".equals(request.getVhost())) {
-            // exchange 허용 케이스
-            // exchange "request" 에 대해서 허용
-            if ("exchange".equals(request.getResource()) && "request".equals(request.getName())) {
-                // 허용하는 permission은 publish뿐
-                List<String> exchangeAllowPermissions = Arrays.asList("write");
+            boolean allow = exchangeAllowPermissions
+                                    .stream()
+                                    .anyMatch(request.getPermission()::equals);
 
-                boolean allow = exchangeAllowPermissions
-                                        .stream()
-                                        .anyMatch(request.getPermission()::equals);
+            // 보내기라면 허용함
+            if (allow) {
+                return "allow";
+            }
+        }
 
-                // 보내기라면 허용함
-                if (allow) {
+        if ("exchange".equals(request.getResource()) && "user".equals(request.getName())) {
+            // 허용하는 permission은 publish뿐
+            List<String> exchangeAllowPermissions = Arrays.asList("read");
+
+            boolean allow = exchangeAllowPermissions
+                                    .stream()
+                                    .anyMatch(request.getPermission()::equals);
+
+            // 보내기라면 허용함
+            if (allow) {
+                return "allow";
+            }
+        }
+
+        // queue 허용 케이스
+        if ("queue".equals(request.getResource())) {
+            String username = request.getUsername();
+            // 허용하는 패턴은 user.[username]
+            Pattern queueNamePattern = Pattern.compile("user." + username);
+            Matcher matcher = queueNamePattern.matcher(request.getName());
+
+            if (matcher.find()) {
+                // 패턴에 맞는다면 큐 생성 허가
+                if ("configure".equals(request.getPermission())) {
                     return "allow";
                 }
-            }
 
-            // queue 허용 케이스
-            if ("queue".equals(request.getResource())) {
-                String username = request.getUsername();
-                // 허용하는 패턴은 user.[username]
-                Pattern queueNamePattern = Pattern.compile("user." + username);
-                Matcher matcher = queueNamePattern.matcher(request.getName());
+                // 패턴에 맞는다면 queue binding 허가
+                if ("write".equals(request.getPermission())) {
+                    return "allow";
+                }
 
-                if (matcher.find()) {
-                    // 패턴에 맞는다면 큐 생성 허가
-                    if ("configure".equals(request.getPermission())) {
-                        return "allow";
-                    }
-
-                    // 패턴에 맞는다면 queue binding 허가
-                    if ("write".equals(request.getPermission())) {
-                        return "allow";
-                    }
-
-                    // 패턴에 맞는다면 consume 허가
-                    if ("read".equals(request.getPermission())) {
-                        return "allow";
-                    }
+                // 패턴에 맞는다면 consume 허가
+                if ("read".equals(request.getPermission())) {
+                    return "allow";
                 }
             }
         }
@@ -99,24 +109,14 @@ public class RabbitAuthController {
 
     @PostMapping(value = "/topic")
     public String postTopic(TopicPathRequest request) {
-        log.info("rabbit auth vhost info: {}", request);
 
-        Pattern pattern = Pattern.compile("(^(chat|command)\\.\\w+)");
+        Pattern topicNamePattern = Pattern.compile("user." + request.getUsername());
+        Matcher matcher = topicNamePattern.matcher(request.getName());
 
-//        if ("write".equals(request.getPermission())) {
-//            if (("chat.user." + username).equals(request.getRoutingKey())) {
-//                return "allow";
-//            }
-//        }
-//
-        if ("buddy".equals(request.getUsername())
-                    && "chat".equals(request.getVhost())
-                    && "topic".equals(request.getResource())
-                    && "request".equals(request.getName())
-                    && "write".equals(request.getPermission())
-                    && (request.getRoutingKey() == null || pattern.matcher(request.getRoutingKey()).find())) {
+        if (matcher.find() && "queue".equals(request.getResource()) && "read".equals(request.getPermission())) {
             return "allow";
         }
+
         return "deny";
     }
 }
